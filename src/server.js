613268fc7120ccb70b0c364c89ca1965b30382b2
Alexander Ramirez <alexanderr375@gmail.com>
2020-09-13 20:19:44 -0400

    Fixing output route for Angular in the server.js

--- src/server.js
+++ src/server.js
@@ -5,7 +5,7 @@ const app = express();
 
 app.use(express.static(__dirname + "../dist/bluelagoon-web"));
 app.get("/", function (req, res) {
-  res.sendFile(path.join(__dirname "../dist/bluelagoon-web/index.html"));
+  res.sendFile(path.join(__dirname + "../dist/bluelagoon-web/index.html"));
 });
 
 app.listen(process.env.PORT || 8080);
